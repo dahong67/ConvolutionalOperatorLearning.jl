c866f46273e7464cdfb02af8b0fddfb7253fe43c
David Hong <dahong67@wharton.upenn.edu>
2019-09-23 07:18:04 -0400

    Change niters to maxiters

--- src/ConvolutionalAnalysisOperatorLearning.jl
+++ src/ConvolutionalAnalysisOperatorLearning.jl
@@ -10,16 +10,16 @@ sosdiff(A,B) = sum(ab -> sosdiff(ab...),zip(A,B))
 hard(x, beta) = abs(x) < beta ? zero(x) : x
 _obj(zlk,λ) = sum(z -> (abs(z) < sqrt(2λ) ? abs2(z)/2 : λ), zlk)
 
-function CAOL(x,h0,  λ;niters=1000,tol=1e-10,trace=false)
-    out = _CAOL(x,h0,λ,  niters,tol,trace)
+function CAOL(x,h0,λ;maxiters=1000,tol=1e-10,trace=false)
+    out = _CAOL(x,h0,λ,maxiters,tol,trace)
     return trace ? out : out[1]
 end
-function CAOL(x,H0,R,λ;niters=1000,tol=1e-10,trace=false)
-    out = _CAOL(x,H0,R,λ,niters,tol,trace)
+function CAOL(x,H0,R,λ;maxiters=1000,tol=1e-10,trace=false)
+    out = _CAOL(x,H0,R,λ,maxiters,tol,trace)
     return trace ? out : out[1]
 end
 
-function _CAOL(x,h0,λ,niters,tol,trace)
+function _CAOL(x,h0,λ,maxiters,tol,trace)
     R, K = size(first(h0)), length(h0)
 
     H0 = similar(first(h0),prod(R),K)
@@ -27,13 +27,13 @@ function _CAOL(x,h0,λ,niters,tol,trace)
         H0[:,k] = vec(h0[k])
     end
 
-    H, (obj,Hdiff), Hs = _CAOL(x,H0,R,λ,niters,tol,trace)
+    H, (obj,Hdiff), Hs = _CAOL(x,H0,R,λ,maxiters,tol,trace)
     h = [reshape(H[:,k],R) for k in 1:K]
 
     return h, (obj,Hdiff), Hs
 end
 
-function _CAOL(x,H0,R,λ,niters,tol,trace)
+function _CAOL(x,H0,R,λ,maxiters,tol,trace)
     @assert H0'H0 ≈ (1/prod(R))*I
     K = size(H0,2)
 
@@ -54,11 +54,11 @@ function _CAOL(x,H0,R,λ,niters,tol,trace)
     UVt = HΨZ
 
     # Initialize trace of H's, objectives and Hdiff
-    Hs = Array{typeof(H0)}(undef,niters)
-    obj   = OffsetArray(fill(NaN,niters),-1)
-    Hdiff = fill(NaN,niters)
+    Hs = Array{typeof(H0)}(undef,maxiters)
+    obj   = OffsetArray(fill(NaN,maxiters),-1)
+    Hdiff = fill(NaN,maxiters)
 
-    for t in 1:niters
+    for t in 1:maxiters
         copyto!(Hprev,H)
 
         # Compute objective and update ΨZ
@@ -86,8 +86,8 @@ function _CAOL(x,H0,R,λ,niters,tol,trace)
         Hdiff[t] <= tol && break
     end
 
-    niter = count(o -> !isnan(o),obj)
-    return H, (obj[0:niter-1],Hdiff[1:niter]), Hs[1:niter]
+    niters = count(o -> !isnan(o),obj)
+    return H, (obj[0:niters-1],Hdiff[1:niters]), Hs[1:niters]
 end
 
 end # module
