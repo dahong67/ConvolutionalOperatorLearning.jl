d91c526b0965d17c29954cfd568ebfb6f8a249b5
David Hong <dahong67@wharton.upenn.edu>
2019-09-22 21:51:53 -0400

    First version using iterables
    Restructured to use iterables, lots of cleanup remains.

--- src/ConvolutionalAnalysisOperatorLearning.jl
+++ src/ConvolutionalAnalysisOperatorLearning.jl
@@ -123,39 +123,57 @@ function _CAOL7(xpad, h0, λ, maxiters, tol, debug)
     end
 end
 
-# Plan
-# 4. Restructure as iterable
+# todos
+# + stopping criterion
+# + parametric types?
 
-function CAOL(x, h0, λ; maxiters = 2000, tol = 1e-13)
-    R, K = size(h0[1]), length(h0)
 
-    H0 = similar(h0[1],prod(R),K) # vectorized form
-    for k in 1:K
-        H0[:,k] = vec(h0[k])
-    end
+import Base: iterate, IteratorSize, IsInfinite, eltype
 
-    return _CAOL(x, H0, R, λ, maxiters, tol)
+struct CAOLIterable
+    x
+    H0
+    R
+    λ
 end
 
-objtrick(zlk,λ) = sum(z -> (abs(z) < sqrt(2λ)) ? abs2(z)/2 : λ, zlk)
+mutable struct CAOLState
+    xpad
 
-function _CAOL(x, H0, R, λ, maxiters, tol)
-    L = length(x)
-    K = size(H0,2)
+    H
+    h
+    Hprev
 
-    # Verify (scaled) orthonormality of H0
-    @assert H0'H0 ≈ (1/prod(R))*I
+    zlk
+    ΨZ
+    ψz
+    ψztemp
+    HΨZ
+    UVt
+
+    # debug: initialization
+    niter
+    H_trace
+    H_convergence
+    obj_fnc_vals
+end
+
+IteratorSize(::Type{<:CAOLIterable}) = IsInfinite()
+eltype(::Type{CAOLIterable}) = CAOLState
+
+function iterate(it::CAOLIterable)
+    K = size(it.H0,2)
 
     # Initialize: padded versions of images
-    xpad = [padarray(xl,Pad(:circular,ntuple(_->0,ndims(xl)),R)) for xl in x]
+    xpad = [padarray(xl,Pad(:circular,ntuple(_->0,ndims(xl)),it.R)) for xl in it.x]
 
     # Initialize: filters
-    H = copy(H0)
-    h = [reshape(view(H,:,k),map(n->1:n,R)) for k in 1:K] # natural form view
-    Hprev = similar(H)                                  # for convergence test
+    H = copy(it.H0)
+    h = [reshape(view(H,:,k),map(n->1:n,it.R)) for k in 1:K] # natural form view
+    Hprev = similar(H)                                       # for convergence test
 
     # Initialize: temporary variables
-    zlk = similar(xpad[1],map(n->0:n-1,size(x[1])))
+    zlk = similar(xpad[1],map(n->0:n-1,size(it.x[1])))
     ΨZ = similar(H)
     ψz = [reshape(view(ΨZ,:,k),axes(h[k])) for k in 1:K]
     ψztemp = similar(ψz[1])
@@ -163,57 +181,68 @@ function _CAOL(x, H0, R, λ, maxiters, tol)
     UVt = HΨZ  # alias to the same memory
 
     # debug: initialization
-    niter = 1;
+    niter = 0;
     H_trace = [];
     H_convergence = [];
     obj_fnc_vals = [];
     # debug
 
-    # Main loop
-    for t in 1:maxiters
-        obj_fnc = 0
+    s = CAOLState(xpad,H,h,Hprev,zlk,ΨZ,ψz,ψztemp,HΨZ,UVt,
+        niter,H_trace,H_convergence,obj_fnc_vals)
+    return s,s
+end
 
-        # Compute ΨZ
-        fill!(ΨZ,zero(eltype(ΨZ)))
-        for l in 1:L, k in 1:K
-            imfilter!(zlk,xpad[l],(h[k],),NoPad(),Algorithm.FIR())
+objtrick(zlk,λ) = sum(z -> (abs(z) < sqrt(2λ)) ? abs2(z)/2 : λ, zlk)
 
-            # debug: calculate the objective function
-            obj_fnc += objtrick(zlk,λ)
-            # debug
+function iterate(it::CAOLIterable,s::CAOLState)
+    L, K = length(it.x), length(s.h)
+    obj_fnc = 0
 
-            zlk .= hard.(zlk,sqrt(2λ))
-            imfilter!(ψztemp,xpad[l],(zlk,),NoPad(),Algorithm.FIR())
-            ψz[k] .+= ψztemp
-        end
+    # Compute ΨZ
+    fill!(s.ΨZ,zero(eltype(s.ΨZ)))
+    for l in 1:L, k in 1:K
+        imfilter!(s.zlk,s.xpad[l],(s.h[k],),NoPad(),Algorithm.FIR())
 
-        # Update filter via polar factorization
-        copyto!(Hprev,H)
-        mul!(HΨZ,H0',ΨZ)   # if we observe drift, may want to use a copy of H0
-        F = svd!(HΨZ)
-        mul!(UVt,F.U,F.Vt)
-        mul!(H,H0,UVt)
-
-        # debug: save outputs
-        push!(H_convergence, normdiff(H,Hprev)/norm(H))
-        push!(H_trace, copy(H))
-        push!(obj_fnc_vals, obj_fnc)
+        # debug: calculate the objective function
+        obj_fnc += objtrick(s.zlk,it.λ)
         # debug
 
-        # Check convergence criteria
-        if (normdiff(H,Hprev)/norm(H) < tol)
-            niter = t
-            break
-        end
-
-        # debug: set niters
-        if t == maxiters
-            niter = t
-        end
-        # debug
+        s.zlk .= hard.(s.zlk,sqrt(2*it.λ))
+        imfilter!(s.ψztemp,s.xpad[l],(s.zlk,),NoPad(),Algorithm.FIR())
+        s.ψz[k] .+= s.ψztemp
     end
 
-    return (h,niter,obj_fnc_vals, H_trace,H_convergence)
+    # Update filter via polar factorization
+    copyto!(s.Hprev,s.H)
+    mul!(s.HΨZ,it.H0',s.ΨZ)
+    F = svd!(s.HΨZ)
+    mul!(s.UVt,F.U,F.Vt)
+    mul!(s.H,it.H0,s.UVt)
+
+    # debug: save outputs
+    s.niter += 1
+    push!(s.H_convergence, normdiff(s.H,s.Hprev)/norm(s.H))
+    push!(s.H_trace, copy(s.H))
+    push!(s.obj_fnc_vals, obj_fnc)
+    # debug
+
+    return s,s
+end
+
+function CAOL(x, h0, λ, niters)
+    R, K = size(h0[1]), length(h0)
+
+    H0 = similar(h0[1],prod(R),K) # vectorized form
+    for k in 1:K
+        H0[:,k] = vec(h0[k])
+    end
+
+    # Verify (scaled) orthonormality of H0
+    @assert H0'H0 ≈ (1/prod(R))*I
+
+    outs = last(collect(Iterators.take(CAOLIterable(x,H0,R,λ),niters+1)))
+
+    return (outs.h,outs.niter,outs.obj_fnc_vals,outs.H_trace,outs.H_convergence)
 end
 
 end # module
