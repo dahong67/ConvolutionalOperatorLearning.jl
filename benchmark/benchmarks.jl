d66f181efb8b936449ab8d7f9d642f7d0fce3d78
David Hong <dahong67@wharton.upenn.edu>
2019-10-09 17:13:23 -0400

    Add benchmark that handcrafts filters

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -10,7 +10,7 @@ rng = MersenneTwister(1)
 x = [randn(rng,128,128) for l in 1:62]
 
 R = (3,3)
-H0 = generatefilters(:DCT,R)[:,2:end]
+H0 = generatefilters(:DCT,R)
 λ = 0.0001
 
 maxiters = 30
@@ -21,4 +21,7 @@ SUITE["CAOL 2D"]["trace,30"]    = @benchmarkable CAOL($x,$λ,($H0,$R),maxiters=3
 SUITE["CAOL 2D"]["notrace,200"] = @benchmarkable CAOL($x,$λ,($H0,$R),maxiters=200)
 SUITE["CAOL 2D"]["trace,200"]   = @benchmarkable CAOL($x,$λ,($H0,$R),maxiters=200,trace=true)
 
+for p in 2:2:size(H0,2)-1
+    SUITE["CAOL 2D"]["p=$p"] = @benchmarkable CAOL($x,$λ,($H0,$R),p=$p,maxiters=30)
+end
 # todo: add more benchmarks, incorporate in package ci appropriately?
